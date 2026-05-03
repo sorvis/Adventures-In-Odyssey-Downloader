4ac4077bc4642cedd7adb2da133deb11e431076a
Steven Orvis <code@huntingpa.info>
2026-05-03 13:45:15 +0000

    Bump version to 0.1.2 for v0.1.2 release

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.odyssey"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.1.1"
+        versionCode = 3
+        versionName = "0.1.2"
     }
     signingConfigs {
         if (keystorePropsFile.exists()) {
