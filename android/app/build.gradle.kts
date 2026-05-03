158a822c764b6e20306281891f3957048681a202
Steven Orvis <code@huntingpa.info>
2026-05-03 20:34:16 +0000

    Bump version to 0.1.8 for v0.1.8 release

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.odyssey"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
-        versionName = "0.1.7"
+        versionCode = 9
+        versionName = "0.1.8"
     }
     signingConfigs {
         if (keystorePropsFile.exists()) {
