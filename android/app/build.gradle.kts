e73410170cdfd61dd2a1a1451ad6db430d67dbc1
Steven Orvis <code@huntingpa.info>
2026-05-05 17:09:49 +0000

    Bump version to 0.1.19 for v0.1.19 release

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.odyssey"
         minSdk = 26
         targetSdk = 35
-        versionCode = 19
-        versionName = "0.1.18"
+        versionCode = 20
+        versionName = "0.1.19"
     }
     signingConfigs {
         if (keystorePropsFile.exists()) {
