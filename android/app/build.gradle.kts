7817065561495520111e05caaa8f272c26413172
Steven Orvis <code@huntingpa.info>
2026-05-05 21:53:21 +0000

    Bump version to 0.1.22 for v0.1.22 release

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.odyssey"
         minSdk = 26
         targetSdk = 35
-        versionCode = 22
-        versionName = "0.1.21"
+        versionCode = 23
+        versionName = "0.1.22"
     }
     signingConfigs {
         if (keystorePropsFile.exists()) {
