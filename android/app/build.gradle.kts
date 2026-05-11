8c3aefb1e4048eca02fa0e292140e9d5182ab598
Steven Orvis <code@huntingpa.info>
2026-05-11 21:21:38 +0000

    Bump version to 0.1.38 for v0.1.38 release

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.odyssey"
         minSdk = 26
         targetSdk = 35
-        versionCode = 38
-        versionName = "0.1.37"
+        versionCode = 39
+        versionName = "0.1.38"
     }
     signingConfigs {
         if (keystorePropsFile.exists()) {
