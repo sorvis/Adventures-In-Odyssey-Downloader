cd42062ae114f8b7d23358b2d470feeba70533b3
Steven Orvis <code@huntingpa.info>
2026-05-24 11:56:19 +0000

    Bump version to 0.1.75 for v0.1.75 release

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.odyssey"
         minSdk = 26
         targetSdk = 35
-        versionCode = 75
-        versionName = "0.1.74"
+        versionCode = 76
+        versionName = "0.1.75"
     }
     signingConfigs {
         if (keystorePropsFile.exists()) {
